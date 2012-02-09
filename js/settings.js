e8fcb2fa921debffa132f6778bfb5d4bc07953e9
Alex <allergencore@gmail.com>
2012-02-09 17:24:02 +0400

    minor changes

--- js/settings.js
+++
@@ -1,9 +0,0 @@
-﻿function _load(){
-document.userName.value=System.Gadget.Settings.readString("username");
-document.pass.value=System.Gadget.Settings.readString("pass");
-document.audio.value=System.Gadget.Settings.readString("audio");
-document.addrRead.value=System.Gadget.Settings.readString("addrRead");
-document.addrSend.value=System.Gadget.Settings.readString("addrSend");
-document.method.value=System.Gadget.Settings.readString("method");
-document.var.value=System.Gadget.Settings.readString("var");
-};
