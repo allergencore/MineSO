1ec5fbb06ca34d49c6de16fb93e1cbcb04aae18e
Alex <allergencore@gmail.com>
2012-03-05 10:19:38 +0400

    major changes

--- js/mineso.js
+++ js/mineso.js
@@ -1,1 +1,48 @@
-﻿var xmlHttpReq = new ActiveXObject("MSXML2.XMLHTTP.3.0");
+﻿function body_onload() {
+//System.Gadget.Flyout.document = "flyout.html";
+System.Gadget.settingsUI = "settings.html";
+refresh();
+}
+
+function refresh() {
+	//System.Gadget.Flyout.show = true;
+	var req = new XMLHttpRequest;
+	var img = document.getElementsByTagName("img")[0];
+	img.setAttribute("src", "img/load.jpg");
+	var ip = System.Gadget.Settings.readString("ip");
+	var port = System.Gadget.Settings.readString("port");
+	if (ip == "" || port == "") {
+		ip = "127.0.0.1";
+		port = 25565;
+	}
+	req.open("POST", "http://mine/sb-status.php", true);
+	req.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+	//req.setRequestHeader("Accept-Charset", "windows-1251");
+	req.send("ip=" + encodeURIComponent(ip) + "&port=" + port);
+	//timeout
+	var sto = setTimeout(function(){
+		req.abort();
+		clearTimeout(sto);
+		clearInterval(sint);
+		delete req;
+	}, 5000);
+	//state change listener
+	var sint = setInterval(function(){
+		if (req.readyState == 4 && req.status == 200) {
+			clearInterval(sint);
+			clearTimeout(sto);
+			switch (req.responseText) {
+				case "up":
+					img.setAttribute("src", "img/up.jpg");
+				break;
+				case "down":
+					img.setAttribute("src", "img/down.jpg");
+				break;
+				default:
+					img.setAttribute("src", "img/err.jpg");
+				break;
+			}
+			delete req;
+		}
+	} , 10);
+}
---
+++ js/settings.js
@@ -0,0 +1,26 @@
+﻿function settings_load() {
+	// Delegate for the settings closing event. 
+	System.Gadget.onSettingsClosing = SettingsClosing;
+	document.getElementById("ip").value = System.Gadget.Setting.readString("ip");
+	document.getElementById("port").value = System.Gadget.Setting.readString("port");
+}
+// --------------------------------------------------------------------
+// Handle the Settings dialog closing event.
+// Parameters:
+// event - event arguments.
+// --------------------------------------------------------------------
+function SettingsClosing(event)
+{
+	// Save the settings if the user clicked OK.
+	if (event.closeAction == event.Action.commit)
+	{
+		var ip = document.getElementById("ip").value;
+		var port = document.getElementById("port").value;
+		System.Gadget.Settings.writeString(
+			"ip", ip);
+		System.Gadget.Settings.writeString(
+			"port", port);
+	}
+	// Allow the Settings dialog to close.
+	event.cancel = false;
+}
